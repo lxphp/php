d32e8816053b236a69e0c6afc2d72ceb0e9e94cf
lxphp <529122692@qq.com>
2017-08-22 19:01:05 +0800

    修改

---
+++ 2.php
@@ -0,0 +1,8 @@
+<?php  
+// @mysql_connect('bdm299714236.my3w.com','bdm299714236','529122692') or die();
+// mysql_query('set name utf8');
+// mysql_query('use bdm299714236_db');
+// $data = mysql_query('select * from article');
+// var_dump($data);
+$str ="hello world ";
+echo substr($str,1,6);
